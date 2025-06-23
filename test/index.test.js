c8a6b077038139e1083a94531aad18d915e3e56d
Arthur Fiorette <me@arthur.place>
2025-06-23 16:58:23 -0300

    feat: add recursiveness tests

--- test/index.test.js
+++ test/index.test.js
@@ -58,3 +58,86 @@ describe('Result', () => {
     assert.strictEqual(typeof new Result({ a: 1 }, null, { a: 1 }).ok, 'boolean');
   });
 });
+
+describe('Result(Result)', () => {
+  // tests all combinations of nested Result objects and ensure they are not inlined
+  test('Result.ok(Result.ok())', () => {
+    const innerResult = Result.ok('inner');
+    const result = Result.ok(innerResult);
+    assert.strictEqual(result.ok, true);
+    assert.strictEqual(result.value, innerResult);
+  });
+
+  test('Result.error(Result.error())', () => {
+    const innerError = Result.error('inner error');
+    const result = Result.error(innerError);
+    assert.strictEqual(result.ok, false);
+    assert.strictEqual(result.error, innerError);
+  });
+
+  test('Result.ok(Result.error())', () => {
+    const innerError = Result.error('inner error');
+    const result = Result.ok(innerError);
+    assert.strictEqual(result.ok, true);
+    assert.strictEqual(result.value, innerError);
+  });
+
+  test('Result.error(Result.ok())', () => {
+    const innerResult = Result.ok('inner');
+    const result = Result.error(innerResult);
+    assert.strictEqual(result.ok, false);
+    assert.strictEqual(result.error, innerResult);
+  });
+
+  test('Result.ok(Result.ok(Result.ok()))', () => {
+    const innerInnerResult = Result.ok('inner inner');
+    const innerResult = Result.ok(innerInnerResult);
+    const result = Result.ok(innerResult);
+    assert.strictEqual(result.ok, true);
+    assert.strictEqual(result.value, innerResult);
+    assert.strictEqual(result.value.ok, true);
+    assert.strictEqual(result.value.value, innerInnerResult);
+  });
+
+  test('Result.error(Result.error(Result.error()))', () => {
+    const innerInnerError = Result.error('inner inner error');
+    const innerError = Result.error(innerInnerError);
+    const result = Result.error(innerError);
+    assert.strictEqual(result.ok, false);
+    assert.strictEqual(result.error, innerError);
+    assert.strictEqual(result.error.ok, false);
+    assert.strictEqual(result.error.error, innerInnerError);
+  });
+
+  test('Result.try(Result.ok())', () => {
+    const innerResult = Result.ok('inner');
+    const result = Result.try(() => innerResult);
+    assert.strictEqual(result.ok, true);
+    assert.strictEqual(result.value, innerResult);
+  });
+
+  test('Result.try(Result.error())', () => {
+    const innerError = Result.error('inner error');
+    const result = Result.try(() => innerError);
+    assert.strictEqual(result.ok, true);
+    assert.strictEqual(result.value, innerError);
+  });
+
+  test('Result.try(throw Result.ok())', () => {
+    const innerResult = Result.ok('inner');
+    const result = Result.try(() => {
+      throw innerResult;
+    });
+    assert.strictEqual(result.ok, false);
+    assert.strictEqual(result.error, innerResult);
+  });
+
+  test('Result.try(throw Result.error())', () => {
+    const innerError = Result.error('inner error');
+    const result = Result.try(() => {
+      throw innerError;
+    });
+    assert.strictEqual(result.ok, false);
+    assert.strictEqual(result.error, innerError);
+  });
+});
